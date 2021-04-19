6d1916b3650e41eb23bda60b2be7c6609a05a87e
Tim Weiland <tim@weiland-lahnstein.de>
2021-04-19 21:59:48 +0200

    Initial commit; overwrite previous repo

---
+++ content-org/.dir-locals.el
@@ -0,0 +1,6 @@
+;;; Patiently waiting for ox-hugo to merge this: https://github.com/kaushalmodi/ox-hugo/pull/401
+((org-mode . (
+              (org-hugo-base-dir . "..")
+              (org-babel-python-command . "~/miniconda3/bin/python")
+              ))
+ ("daily" . ((org-hugo-base-dir . nil))))
